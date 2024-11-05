c73196394d98e7dac26d79808753579e6b906aab
ahmed Radwan <ahmedradwandevelop@gmail.com>
2024-11-05 16:23:40 +0100

    first day task 3

---
+++ day-1/SessionLiveTasks/task3.dart
@@ -0,0 +1,17 @@
+void main() {
+  int numberIntegar = 7;
+  double numberFraction = 4.5;
+  String numberString = "10";
+
+  print(numberIntegar);
+  print(numberFraction);
+  print(numberString);
+
+  numberIntegar = 17;
+  numberFraction = 20.6;
+  numberString = "25";
+  print("-------------------------------------");
+  print(numberIntegar);
+  print(numberFraction);
+  print(numberString);
+}
