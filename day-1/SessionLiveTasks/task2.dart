25a6720768e7151291762cfbc5b1853a4d5d99e7
ahmed Radwan <ahmedradwandevelop@gmail.com>
2024-11-05 16:23:09 +0100

    first day task 2

---
+++ day-1/SessionLiveTasks/task2.dart
@@ -0,0 +1,9 @@
+void main() {
+  ///multiline string
+  String multipleLineString = """
+          hello every one
+          my name is ahmed
+          i'm start to learn flutter
+  """;
+  print(multipleLineString);
+}
