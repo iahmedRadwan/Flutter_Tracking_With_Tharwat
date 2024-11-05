d2ccaaf236e992e16991a86c70d3abd017d3b5f7
ahmed Radwan <ahmedradwandevelop@gmail.com>
2024-11-05 16:24:49 +0100

    first day task 5

---
+++ day-1/SessionLiveTasks/task5.dart
@@ -0,0 +1,13 @@
+void main() {
+  // if decleration only it will be error
+  // so we will intalization a variable
+  int number = 5;
+  number += 5;
+  print("addition operation : " + number.toString());
+  number -= 3;
+  print("subtractiom operation : " + number.toString());
+  number *= 5;
+  print("Multiplication operation : " + number.toString());
+  var number2 = number / 5.0;
+  print("Division operation : " + number2.toString());
+}
