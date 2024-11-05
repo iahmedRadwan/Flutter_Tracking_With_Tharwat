fed0f9c4c29fa3474055bfc3217cfca3298c8466
ahmed Radwan <ahmedradwandevelop@gmail.com>
2024-11-05 16:25:42 +0100

    first day task 7

---
+++ day-1/SessionLiveTasks/task7.dart
@@ -0,0 +1,9 @@
+void main() {
+  int numberIntegar = 7;
+  double numberFraction = 5.5;
+  num number, number2;
+  number = numberIntegar;
+  number2 = numberFraction;
+  var result = number + number2;
+  print(result);
+}
