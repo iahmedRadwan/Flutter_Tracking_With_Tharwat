b256203783c0cfde9b626b0dbd254b0cf15e8d4d
ahmed Radwan <ahmedradwandevelop@gmail.com>
2024-11-06 04:50:22 +0100

    done create assignment 3 day 2

---
+++ day-2/Assignments/assignment3.dart
@@ -0,0 +1,13 @@
+// Set Properties
+// Objective: Learn about sets by checking properties like size and membership.
+// Instructions:
+// - Create a set named uniqueNumbers with the values {10, 20, 30, 40}.
+// - Print the length of uniqueNumbers.
+// - Check if the number 20 is present in the set and print the result.
+
+void main() {
+  Set<int> uniqueNumbers = {10, 20, 30, 40};
+  print(uniqueNumbers.length);
+  bool isContainValue20 = uniqueNumbers.contains(20);
+  print(isContainValue20);
+}
