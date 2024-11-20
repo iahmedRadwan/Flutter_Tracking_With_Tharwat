5e217edeb53fb2b2e30d5ad0174fe60d175fcab1
ahmed Radwan <ahmedradwanflutter@gmail.com>
2024-11-20 18:59:17 +0100

    change place of getcode to be sperated in folder

---
+++ leetCode/tharwatProblems/letCodeQuestion1.dart
@@ -0,0 +1,41 @@
+void main() {
+  print(Solution().containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+}
+
+class Solution {
+  /// Error Time Limit Exceeded
+  // bool containsDuplicate(List<int> nums) {
+  //   List<int> filteredNums = [...nums];
+  //   bool isNumContainsDuplicate = false;
+  //   for (int i = 0; i < nums.length; i++) {
+  //     filteredNums.remove(nums[i]);
+  //     if (filteredNums.contains(nums[i])) {
+  //       isNumContainsDuplicate = true;
+  //       break;
+  //     }
+  //   }
+  //   return isNumContainsDuplicate;
+  // }
+
+  /// Accepted with me
+  // bool containsDuplicate(List<int> nums) {
+  //   bool isNumContainsDuplicate = false;
+  //   nums.sort();
+  //   for (int i = 0; i < nums.length - 1; i++) {
+  //     if (nums[i] == nums[i + 1]) {
+  //       isNumContainsDuplicate = true;
+  //       break;
+  //     }
+  //   }
+  //   return isNumContainsDuplicate;
+  // }
+
+  /// After watch video in youtube
+  bool containsDuplicate(List<int> nums) {
+    if (nums.toSet().length < nums.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
